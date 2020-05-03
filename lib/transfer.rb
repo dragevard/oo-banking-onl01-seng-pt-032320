c869c845ff7e0c9eb71f0cd642180fbbafc2bdc9
Derek phung <dragevards@gmail.com>
2020-05-03 23:17:22 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,6 +18,20 @@ class Transfer
   end 
   
   def execute_transaction 
-    if @status == "pending"
+    if @status == "pending" && @sender.balance > @amount && @receiver.valid? && sender.valid 
+      @sender.balance -= @amount 
+      @receiver.balance += @amount 
+      @status = "complete"
+    else 
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end 
+  end 
+  
+  def reverse_transfer 
+    if status == "complete"
+      @sender.balance += @amount 
+      
+      
       
 end
