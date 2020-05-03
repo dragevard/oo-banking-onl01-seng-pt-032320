3acc84a29f1d6873681e405ac967e13a3da66489
Derek phung <dragevards@gmail.com>
2020-05-03 23:20:18 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,14 +24,14 @@ class Transfer
       @status = "complete"
     else 
       @status = "rejected"
-      "Transaction rejected. Please check your account balance."
+      "Transaction rejected. Please check your account balance"
     end 
   end 
   
   def reverse_transfer 
     if @status == "complete"
       @sender.balance += @amount 
-      @receiver.balance == @amount 
+      @receiver.balance -= @amount 
       @status = "reversed"
     end 
   end 
